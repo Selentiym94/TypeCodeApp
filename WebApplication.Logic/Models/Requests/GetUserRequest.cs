5b5b801fe6ab9e6aa5029382d646783afca08554
Student <Student@KVANT-29>
2024-03-02 14:57:52 +0300

    реализация интерфейса клиента + интерфейс для запросов

---
+++ WebApplication.Logic/Models/Requests/GetUserRequest.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication.Logic.Interfaces;
+
+namespace WebApplication.Logic.Models.Requests
+{
+    public class GetUserRequest : ITypeCodeRequest
+    {
+        public GetUserRequest(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public string GetUrlParams()
+        {
+            string result = "users";
+            if (!string.IsNullOrEmpty(Name))
+            {
+                result += $"?name={Name}";
+            }
+            return result;
+        }
+    }
+}
---
+++ WebApplication.Logic/Processors/TypecodeClient.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using WebApplication.Logic.Interfaces;
+using WebApplication.Logic.Models;
+
+namespace WebApplication.Logic.Processors
+{
+    public class TypecodeClient: ITypecodeClient
+    {
+        private readonly string _baseUrl;
+
+        public TypecodeClient(string baseUrl)
+        {
+            _baseUrl = baseUrl;
+        }
+
+        public List<Album> GetAlbums(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetComments(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Note> GetNotes(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Photo> GetPhotos(int albumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> GetPosts(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetUser(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<List<TData>> GetData<TData>(ITypeCodeRequest request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string urlData = request.GetUrlParams();
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Method = HttpMethod.Get;
+                message.RequestUri = new Uri($"{_baseUrl}/{urlData}");
+
+                HttpResponseMessage response = await client.SendAsync(message);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<List<TData>>(content) ?? new List<TData>();
+                }
+                return new List<TData>();
+            }
+
+        }
+    }
+}
