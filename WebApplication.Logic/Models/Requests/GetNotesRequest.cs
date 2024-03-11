e096db9ec7502717ae64eb614d5c41f1967553e1
ghgtv <zahtr@mail.ru>
2024-03-11 19:43:59 +0300

    Запрос по заметкам

---
+++ WebApplication.Logic/Models/Requests/GetNotesRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Logic.Interfaces;
+
+namespace WebApplication.Logic.Models.Requests
+{
+    public class GetNotesRequest : ITypeCodeRequest
+    {
+        public GetNotesRequest(int userId)
+        {
+            UserId = userId;
+        }
+        public int UserId { get; set; }
+        public string GetUrlParams()
+        {
+            string result = "todos";
+            if (UserId != 0)
+            {
+                result += $"?userId={UserId}";
+            }
+            return result;
+        }
+    }
+}
