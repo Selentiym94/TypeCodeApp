4e49977f83fe10190c9cde5c3814b1df5362f388
Student <Student@KVANT-29>
2024-03-23 14:32:19 +0300

    доделали процессор

--- WebApplication.Logic/Processors/DataProcessor.cs
+++ WebApplication.Logic/Processors/DataProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using WebApplication.Logic.Interfaces;
 using WebApplication.Logic.Models;
 
@@ -18,11 +19,15 @@ namespace WebApplication.Logic.Processors
 
         public async Task<TypeCodeData> Get(string userName)
         {
-
-            User user = (await _client.GetUser(userName))[0];
-            List<Note> notes = await _client.GetNotes(user.Id);
+            TypeCodeData typeCodeData = new TypeCodeData();
+            typeCodeData.User = (await _client.GetUser(userName))[0];
+            typeCodeData.Notes = await _client.GetNotes(typeCodeData.User.Id);
+            typeCodeData.AlbumDats = await GetAlbumData(typeCodeData.User.Id);
+            typeCodeData.PostDats = await GetPostData(typeCodeData.User.Id);
+            return typeCodeData;
 
         }
+
         private async Task<List<AlbumData>> GetAlbumData(int userId)
         {
             List<AlbumData> result = new List<AlbumData>();
@@ -31,6 +36,18 @@ namespace WebApplication.Logic.Processors
             {
                 List<Photo> photos = await _client.GetPhotos(userId);
                 AlbumData data = new AlbumData(album, photos);
+                result.Add(data);
+            }
+            return result;
+        }
+        private async Task<List<PostData>> GetPostData(int userId)
+        {
+            List<PostData> result = new List<PostData>();
+            List<Post> posts = await _client.GetPosts(userId);
+            foreach (Post post in posts)
+            {
+                List<Comment> comments = await _client.GetComments(userId);
+                PostData data = new PostData(post, comments);
                 result.Add(data);
             }
             return result;
